03a4483be9de54263912e5b595d0e00618108fe6
kvnprog <kevin_mundo@hotmail.com>
2022-01-27 15:08:25 -0600

    cambios

--- lib/data/places_array_data_class.dart
+++ lib/data/places_array_data_class.dart
@@ -1,23 +1,30 @@
 //here are all places available in array form
 import 'package:flutter/material.dart';
 import 'package:recorridos_app/data/data.dart';
+import 'package:http/http.dart' as http;
 
-class PlacesArrayAvailableData{
+class PlacesArrayAvailableData {
   List<Places> arrayPlaces = [];
 
-  PlacesArrayAvailableData(){
-    arrayPlaces.add(Places(icon: Icons.fastfood, name: 'Cocina', isActive: false, timeStart: null, timeEnd: null, numeroDeIncidencias: 0));
-    arrayPlaces.add(Places(icon: Icons.monitor_sharp, name: 'Oficina', isActive: false, timeStart: null, timeEnd: null, numeroDeIncidencias: 0));
-    arrayPlaces.add(Places(icon: Icons.filter_alt_sharp, name: 'Filtro', isActive: false, timeStart: null, timeEnd: null, numeroDeIncidencias: 0));
-    arrayPlaces.add(Places(icon: Icons.medical_services_rounded, name: 'Consultorio', isActive: false, timeStart: null, timeEnd: null, numeroDeIncidencias: 0));
-    arrayPlaces.add(Places(icon: Icons.auto_awesome, name: 'Calidad', isActive: false, timeStart: null, timeEnd: null, numeroDeIncidencias: 0));
-    arrayPlaces.add(Places(icon: Icons.commute_sharp, name: 'Estacionamiento', isActive: false, timeStart: null, timeEnd: null, numeroDeIncidencias: 0));
+  PlacesArrayAvailableData() {
+    arrayPlaces.add(Places(
+        icon: Icons.fastfood,
+        name: 'recorrido',
+        isActive: false,
+        timeStart: null,
+        timeEnd: null,
+        numeroDeIncidencias: 0));
 
     showValuesArray();
   }
 
-  List<Places> showValuesArray(){
-    return arrayPlaces;
+  void inicializar(codigo) async {
+    var url =
+        Uri.parse("https://pruebasmatch.000webhostapp.com/traer_lugares.php");
+    var resultado = await http.post(url, body: {"codigo": codigo});
   }
 
+  List<Places> showValuesArray() {
+    return arrayPlaces;
+  }
 }
