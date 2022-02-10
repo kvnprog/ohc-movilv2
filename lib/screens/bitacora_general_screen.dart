f0eb16a8eb57a81681b84cd6a80a9a4ba3fcd69c
Charly Cardoso <charlycardoso.dev@gmail.com>
2022-02-10 08:10:41 -0600

    filters complete

--- lib/screens/bitacora_general_screen.dart
+++ lib/screens/bitacora_general_screen.dart
@@ -1,165 +1,63 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:recorridos_app/data/data.dart';
 import 'package:recorridos_app/screens/screens.dart';
 import 'package:recorridos_app/widgets/list_widget.dart';
 import 'package:recorridos_app/widgets/widgets.dart';
 import 'package:http/http.dart' as http;
 
-class BitacoraGeneral extends StatelessWidget {
+class BitacoraGeneral extends StatefulWidget {
   List userArray;
   String user;
   dynamic userName;
   String? codigo;
 
-  ConectionData connect = ConectionData();
 
   BitacoraGeneral(
-      {required this.userArray,
+    {required this.userArray,
       required this.userName,
       required this.user,
-      this.codigo});
+      this.codigo
+    });
+
+  @override
+  State<BitacoraGeneral> createState() => _BitacoraGeneralState();
+
+  dynamic nameValue;
+  dynamic hourValue;
+  
+  dynamic isOne(int index){
+    switch(index){
+      case 0: {
+        return nameValue;
+      }
+      case 1: {
+        return hourValue;
+      }
+    }
+  }
+}
+
+class _BitacoraGeneralState extends State<BitacoraGeneral> {
+  ConectionData connect = ConectionData();
+  
   @override
   Widget build(BuildContext context) {
-    List arrayList = [
-      {
-        'nombre': 'Manuel Neuer',
-        'actividad': [
-          {
-            'title': 'checkpoints',
-            'checkpoint': [
-              {'hora': '10:00'},
-              {'hora': '20:00'},
-              {'hora': '18:00'},
-            ]
-          },
-          {
-            'title': 'incidencias',
-            'incidence': [
-              {'hora': '10:00', 'lugar': 'cocina'},
-              {'hora': '20:00', 'lugar': 'oficina'},
-              {'hora': '18:00', 'lugar': 'consultorio'},
-            ],
-          }
-        ]
-      },
-      {
-        'nombre': 'Christian Martinoli',
-        'actividad': [
-          {
-            'title': 'checkpoints',
-            'checkpoint': [
-              {'hora': '12:00'},
-              {'hora': '00:00'},
-              {'hora': '15:00'},
-            ]
-          },
-          {
-            'title': 'incidencias',
-            'incidence': [
-              {'hora': '2:00', 'lugar': 'estacionamiento'},
-              {'hora': '21:00', 'lugar': 'baño'},
-              {'hora': '8:00', 'lugar': 'losa'},
-            ],
-          }
-        ]
-      },
-      {
-        'nombre': 'Luis García',
-        'actividad': [
-          {
-            'title': 'checkpoints',
-            'checkpoint': [
-              {'hora': '20:00'},
-              {'hora': '2:00'},
-              {'hora': '7:00'},
-            ]
-          },
-          {
-            'title': 'incidencias',
-            'incidence': [
-              {'hora': '21:00', 'lugar': 'patio'},
-              {'hora': '20:00', 'lugar': 'estacionamiento'},
-              {'hora': '2:00', 'lugar': 'gerencia'},
-            ],
-          }
-        ]
-      },
-      {
-        'nombre': 'Francesco Toti',
-        'actividad': [
-          {
-            'title': 'checkpoints',
-            'checkpoint': [
-              {'hora': '17:00'},
-              {'hora': '00:00'},
-              {'hora': '4:00'},
-            ]
-          },
-          {
-            'title': 'incidencias',
-            'incidence': [
-              {'hora': '3:00', 'lugar': 'almacén'},
-              {'hora': '12:00', 'lugar': 'estacionamiento'},
-              {'hora': '8:00', 'lugar': 'sala'},
-            ],
-          }
-        ]
-      },
-      {
-        'nombre': 'Roberto Carlos',
-        'actividad': [
-          {
-            'title': 'checkpoints',
-            'checkpoint': [
-              {'hora': '10:40'},
-              {'hora': '20:20'},
-              {'hora': '18:10'},
-            ]
-          },
-          {
-            'title': 'incidencias',
-            'incidence': [
-              {'hora': '10:09', 'lugar': 'sala'},
-              {'hora': '20:50', 'lugar': 'cuartos'},
-              {'hora': '18:20', 'lugar': 'planta'},
-            ],
-          }
-        ]
-      },
-      {
-        'nombre': 'Zinedine Zidane',
-        'actividad': [
-          {
-            'title': 'checkpoints',
-            'checkpoint': [
-              {'hora': '10:10'},
-              {'hora': '20:20'},
-              {'hora': '18:10'},
-            ]
-          },
-          {
-            'title': 'incidencias',
-            'incidence': [
-              {'hora': '10:20', 'lugar': 'consultorio'},
-              {'hora': '20:30', 'lugar': 'estacionamiento'},
-              {'hora': '18:20', 'lugar': 'patio'},
-            ],
-          }
-        ]
-      },
-    ];
+    dynamic filtro1;
+    dynamic filtro2;
+  
     Future<String> bitacora() async {
       var url = Uri.parse("${connect.serverName()}traer_bitacora.php");
-      var resultado = await http.post(url, body: {"codigo": codigo});
+      var resultado = await http.post(url, body: {"codigo": widget.codigo});
 
       return resultado.body;
     }
 
     Future<String> usuarios() async {
       var url = Uri.parse("${connect.serverName()}traer_usuarios.php");
-      var resultado = await http.post(url, body: {"codigo": codigo});
+      var resultado = await http.post(url, body: {"codigo": widget.codigo});
 
       return resultado.body;
     }
@@ -181,10 +79,8 @@ class BitacoraGeneral extends StatelessWidget {
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
                       // print(snapshot.data);
-                      List<dynamic> usuarios =
-                          jsonDecode(snapshot.data.toString());
+                      List<dynamic> usuarios = jsonDecode(snapshot.data.toString());
 
-                      print(usuarios[0][0]);
                       return Column(
                         children: [
                           SizedBox(
@@ -196,11 +92,9 @@ class BitacoraGeneral extends StatelessWidget {
                                   scrollDirection: Axis.horizontal,
                                   shrinkWrap: true,
                                   children: [
-                                    filterWidget(
-                                        filtro1, 0, 'Nombre', usuarios),
+                                    filterWidget(filtro1, 0, 'Nombre', usuarios, 0),
                                     const SizedBox(width: 20),
-                                    filterWidget(filtro2, 0, 'Hora',
-                                        [1, 2, 8, 12, 24, 48, 72]),
+                                    filterWidget(filtro2, 0, 'Hora', [1, 2, 8, 12, 24, 48, 72], 1),
                                   ],
                                 ),
                               )),
@@ -268,31 +162,9 @@ class BitacoraGeneral extends StatelessWidget {
     );
   }
 
-  List<DropdownMenuItem<String>> getItemsDropDown(List<dynamic> datos) {
-    List<DropdownMenuItem<String>> itemsAvailable = [];
+  Widget filterWidget( var filterName, int index, String filterTitle, List<dynamic> datos, int context) {
 
-    datos.forEach((element) {
-      itemsAvailable.add(DropdownMenuItem(
-        child: Text('${element.toString()}'),
-        value: element.toString(),
-      ));
-    });
-
-    // datos.forEach((key, value) {
-    //   itemsAvailable.add(DropdownMenuItem(
-    //     child: Text('${value.toString()}'),
-    //     value: value,
-    //   ));
-    // });
-
-    return itemsAvailable;
-  }
-
-  dynamic filtro1;
-  dynamic filtro2;
-  Widget filterWidget(
-      var filterName, int index, String filterTitle, List<dynamic> datos) {
-    // print('yo soy el filtro $filterName');
+    List<dynamic> mList = [widget.nameValue, widget.hourValue];
 
     return Container(
       margin: const EdgeInsets.only(top: 20),
@@ -303,16 +175,16 @@ class BitacoraGeneral extends StatelessWidget {
             style: const TextStyle(color: Colors.white),
           ),
           Container(
-            height: 40,
-            width: 200,
+            height: 50,
+            width: 220,
             decoration: BoxDecoration(
                 color: Colors.grey[600],
                 borderRadius: const BorderRadius.all(Radius.circular(3))),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: DropdownButton<String>(
-                  value: filterName,
-                  items: getItemsDropDown(datos),
+                  value: mList[context],
+                  items: getItemsDropDown(datos, filterTitle),
                   //items: getItemsDropDown(index, filterTitle),
                   borderRadius: const BorderRadius.all(Radius.circular(10)),
                   dropdownColor: Colors.amber[200],
@@ -323,11 +195,54 @@ class BitacoraGeneral extends StatelessWidget {
                   underline: Container(
                     color: Colors.white,
                   ),
-                  onChanged: (opt) {}),
+                  onChanged: (opt) {
+                    setState(() {
+                      switch(context){
+                        case 0: {
+                          widget.nameValue = opt;
+                        }
+                        break;
+                        case 1: {
+                          widget.hourValue = opt;
+                        }
+                        break;
+
+                      }
+                    });
+                  }),
             ),
           ),
         ],
       ),
     );
+  }
+
+    List<DropdownMenuItem<String>> getItemsDropDown(List<dynamic> datos, String context) {
+    dynamic mValue = 'Todos';
+    
+    List<DropdownMenuItem<String>> itemsAvailable = [];
+
+    datos.forEach((element) {
+    switch(context){
+          case 'Nombre':{
+            mValue = element[0];
+          }
+          break;
+
+          case 'Hora':{
+            mValue = element.toString();
+          }
+          break;
+    }
+
+      itemsAvailable.add(DropdownMenuItem(
+        child: Text(mValue),
+        value: mValue
+      ));
+    });
+
+    
+
+    return itemsAvailable;
   }
 }
