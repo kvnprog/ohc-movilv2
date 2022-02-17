7e62513fb5c352048edf2c0de9d3cbcd41da3505
Charly Cardoso <charlycardoso.dev@gmail.com>
2022-02-17 10:36:59 -0600

    changes

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:recorridos_app/data/places_array_data_class.dart';
 import 'package:recorridos_app/screens/screens.dart';
@@ -40,153 +38,172 @@ class _MainClassState extends State<MainClass> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () {
-        return Future(() => false);
-      },
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Home',
-        home: Scaffold(
-          appBar: AppBar(
-            actions: [
-              Row(
-                children: [
-                  const Text(
-                    'Salir',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  IconButton(
-                      onPressed: () async {
-                        var url = Uri.parse(
-                            "${widget.conectionData.serverName()}crear_salida.php");
-                        var entrada = await http
-                            .post(url, body: {'index': widget.entrada});
-                        print(entrada.body);
-                        Navigator.of(context).pop('login');
-                      },
-                      icon: const Icon(
-                        Icons.logout,
-                        color: Colors.black,
-                        size: 30,
-                      )),
-                ],
-              ),
-            ],
-            title: const Text('Home'),
-          ),
-          body: Container(
-                margin: const EdgeInsets.only(top: 30),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-
-                        const SizedBox(width: 15,),
-
-                        /*      GestureDetector(
-                          child: Column(
-                            children: const [
-                              Icon(
-                                Icons.place,
-                                size: 65,
-                                color: Colors.red,
-                              ),
-                              Text(
-                                'Ubicaciones',
-                                style: TextStyle(fontSize: 10, color: Colors.white),
-                              )
-                            ],
-                          ),
-                          onTap: () => Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return HomeToursScreen(
-                                isFromMenu: true,
-                                acciones: widget.acciones,
-                                usuario: widget.usuario,
-                                nombre: widget.nombre[0],
-                                dataList: widget.dataList,
-                                entrada: widget.entrada);
-                          })),
-                        ),
-*/
-                        
-                        GestureDetector(
-                          child: Column(
-                            children: const [
-                              Icon(Icons.file_copy, size: 65, color: Colors.white),
-                              Text(
-                                'Bitácora de incidencias',
-                                overflow: TextOverflow.visible,
-                                softWrap: false,
-                                maxLines: 1,
-                                style: TextStyle(
-                                  fontSize: 10,
-                                  fontFamily: 'Roboto',
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              )
-                            ],
-                          ),
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext context) => ListWidget(
-                                      codigo: widget.codigo,
-                                    )));
-                          },
-                        ),
-
-                        const SizedBox(width: 20,),
-
-                        GestureDetector(
-                          child: Column(
-                            children: const [
-                              Icon(
-                                Icons.file_present_rounded,
-                                size: 65,
-                                color: Colors.white,
-                              ),
-                              Text(
-                                'Bitácora de actividades',
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: null,
-                                semanticsLabel: '...',
-                                textWidthBasis: TextWidthBasis.parent,
-                                style: TextStyle(fontSize: 10, color: Colors.white),
-                              )
-                            ],
-                          ),
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext context) => BitacoraGeneral(
-                                    userArray: userArray,
-                                    user: widget.usuario.toString(),
-                                    userName: widget.nombre,
-                                    codigo: widget.codigo
-                                    //codigo: widget.codigo
-                                    )));
-                          },
-                        ),
-                      ],
-                    ),
-
-                     ListView(
-                       shrinkWrap: true,
-                       children: [
-                         InteractionMenu(nombre: widget.nombre.toString(), acciones: widget.acciones!, isNewMenuRequest: true, btnsave: true, tipo: "1", func: (){setState(() {});}),
-                       ],
-                     )
-                  ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Home',
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Row(
+              children: [
+                const Text(
+                  'Salir',
+                  style: TextStyle(color: Colors.black),
                 ),
-              ),
+                IconButton(
+                    onPressed: () async {
+                      var url = Uri.parse(
+                          "${widget.conectionData.serverName()}crear_salida.php");
+                      var entrada = await http
+                          .post(url, body: {'index': widget.entrada});
+                      print(entrada.body);
+                      Navigator.of(context).pop('login');
+                    },
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Colors.black,
+                      size: 30,
+                    )),
+              ],
+            ),
+          ],
+          title: const Text('Home'),
+          
         ),
-        theme: ThemeData(
-            scaffoldBackgroundColor: Colors.grey[850],
-            appBarTheme: const AppBarTheme(backgroundColor: Colors.amber),
-            floatingActionButtonTheme: const FloatingActionButtonThemeData(
-                backgroundColor: Colors.amber)),
+        body: Column(
+          children: [
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                
+                GestureDetector(
+                  child: Column(
+                    children: const [
+                      Icon(Icons.file_copy, size: 65, color: Colors.white),
+                      Text(
+                        'Incidencias',
+                        overflow: TextOverflow.visible,
+                        softWrap: false,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) => ListWidget(
+                              codigo: widget.codigo,
+                            )));
+                  },
+                ),
+
+                GestureDetector(
+                  child: Column(
+                    children: const [
+                      Icon(
+                        Icons.file_present_rounded,
+                        size: 65,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Actividades',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: null,
+                        semanticsLabel: '...',
+                        textWidthBasis: TextWidthBasis.parent,
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      )
+                    ],
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) => BitacoraGeneral(
+                            userArray: userArray,
+                            user: widget.usuario.toString(),
+                            userName: widget.nombre,
+                            codigo: widget.codigo
+                            //codigo: widget.codigo
+                            )));
+                  },
+                ),
+              
+                GestureDetector(
+                  child: Column(
+                    children: const [
+                      Icon(
+                        Icons.emoji_flags_sharp,
+                        size: 65,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'CheckPoint',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: null,
+                        semanticsLabel: '...',
+                        textWidthBasis: TextWidthBasis.parent,
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      )
+                    ],
+                  ),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context){
+                      return Center(
+                        child: Container(
+                          width: 350,
+                          height: 300,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: Column(
+                            children: [
+
+                              Container(
+                                margin: const EdgeInsets.only(top: 10, bottom: 40),
+                                child: const Text('Crear CheckPoint', style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  textBaseline: null,
+                                  fontStyle: FontStyle.normal,
+                                  decoration: TextDecoration.none,
+                                  fontWeight: FontWeight.normal
+                                ),
+                                textAlign: TextAlign.center,
+                                ),
+                              ),
+
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+                  },
+                  
+                ),
+              
+              ],
+            ),
+
+             ListView(
+               shrinkWrap: true,
+               children: [
+                 InteractionMenu(nombre: widget.nombre.toString(), acciones: widget.acciones!, isNewMenuRequest: true, btnsave: true, tipo: "1", func: (){setState(() {});}),
+               ],
+             )
+          ],
+        ),
       ),
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.grey[850],
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.amber),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.amber)),
     );
   }
 
