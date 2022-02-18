44bc91ca172eaeaa4ba16ae46aa97a74a8b9937c
Charly Cardoso <charlycardoso.dev@gmail.com>
2022-02-18 13:01:23 -0600

    fixes

--- lib/widgets/list_widget.dart
+++ lib/widgets/list_widget.dart
@@ -55,7 +55,7 @@ class _ListWidgetState extends State<ListWidget> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bitácora de recorridos'),
+        title: const Text('Bitácora de Incidencias'),
         elevation: 0,
       ),
       body: Container(
